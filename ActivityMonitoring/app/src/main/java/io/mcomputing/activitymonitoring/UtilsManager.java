3c12566a0795cf8567329996f98a8b82294d4d21
Harald Deutschmann <harald.deutschmann@student.tugraz.at>
2019-04-24 09:57:17 +0200

    #1: acitivty monitoring and feature extraction

--- ActivityMonitoring/app/src/main/java/io/mcomputing/activitymonitoring/UtilsManager.java
+++ ActivityMonitoring/app/src/main/java/io/mcomputing/activitymonitoring/UtilsManager.java
@@ -1,10 +1,28 @@
 package io.mcomputing.activitymonitoring;
+import android.content.Context;
 import android.graphics.Color;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.util.Log;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.nio.ByteBuffer;
 import java.security.SecureRandom;
+import java.util.List;
 import java.util.Random;
 
 public class UtilsManager {
@@ -29,6 +47,94 @@ public class UtilsManager {
 		buffer.put(bytes);
 		buffer.flip();//need flip
 		return buffer.getLong();
+	}
+
+	public static void writeFile(Context context, String title, List<String> lines) {
+		DataOutputStream fOutStream = null;
+		String newLine = System.getProperty("line.separator");
+		String path = context.getFilesDir().getPath() + '/' + title;
+		File newFile = new File(path);
+		Log.d("RESPONSE", "1");
+
+		if(!newFile.exists()) {
+			Log.d("RESPONSE", "2");
+			try {
+				boolean createFile = newFile.createNewFile();
+				if(!createFile)
+					return;
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		try {
+
+			fOutStream = new DataOutputStream(new FileOutputStream(path));
+			for (String line : lines) {
+				fOutStream.writeBytes(line);
+				fOutStream.writeBytes(newLine);
+				fOutStream.flush();
+			}
+			Log.d("RESPONSE", "4");
+			uploadFile(context, path);
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+
+		} finally {
+			try {
+				if (fOutStream != null) {
+					fOutStream.close();
+				}
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+		}
+
+	}
+
+	private static void uploadFile(Context context, String title){
+		FirebaseStorage storage = FirebaseStorage.getInstance(context.getString(R.string.firebase_storage));
+		StorageReference storageRef = storage.getReference();
+		StorageReference activityRef = storageRef.child("activity");
+		DataInputStream dataInputStream = null;
+		try {
+			dataInputStream = new DataInputStream(new FileInputStream(title));
+			UploadTask uploadTask = activityRef.putStream(dataInputStream);
+			uploadTask.addOnFailureListener(new OnFailureListener() {
+				@Override
+				public void onFailure(@NonNull Exception exception) {
+					// Handle unsuccessful uploads
+					Log.d("RESPONSE", "error:" + exception.getMessage());
+
+				}
+			}).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+				@Override
+				public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+					Log.d("RESPONSE", "success:" + taskSnapshot.toString());
+					// taskSnapshot.getMetadata() contains file metadata such as size, content-type, etc.
+					// ...
+				}
+			});
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public static long stringToLong(String name){
+		long h = 1125899906842597L; // prime
+		int len = name.length();
+
+		for (int i = 0; i < len; i++) {
+			h = 31*h + name.charAt(i);
+		}
+		return h;
 	}
 
 	public static int getRandomColor(){
